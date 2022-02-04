b056664b7580185bf46b97758e38d400b4b0a635
Uítalo <uitalo.urss@gmail.com>
2022-02-03 23:12:23 -0300

    modulando o codigo javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-const sr = ScrollReveal({
-  reset: true
-});
+import { initScrollReveal } from "./scrollReveal.js";
 
-sr.reveal(".toDown", {
-  origin: "top",
-  distance: "50px",
-  duration: 2000,
-  delay: 300,
-  reset: false
-});
-
-sr.reveal(".toLeft", {
-  origin: "left",
-  distance: "50px",
-  duration: 2000,
-  delay: 400,
-  reset: false
-});
-
-sr.reveal(".toRight", {
-  origin: "right",
-  distance: "50px",
-  duration: 2000,
-  delay: 500,
-  reset: false
-});
+initScrollReveal();
--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,12 +1,29 @@
 export function initScrollReveal(){
   const sr = ScrollReveal({
+    reset: true
+  });
+  
+  sr.reveal(".toDown", {
     origin: "top",
     distance: "50px",
     duration: 2000,
+    delay: 300,
+    reset: false
   });
-
-  sr.reveal(".toDown", {
-    delay: 5000,
+  
+  sr.reveal(".toLeft", {
+    origin: "left",
+    distance: "50px",
+    duration: 2000,
+    delay: 400,
+    reset: false
+  });
+  
+  sr.reveal(".toRight", {
+    origin: "right",
+    distance: "50px",
+    duration: 2000,
+    delay: 500,
+    reset: false
   });
 }
-
