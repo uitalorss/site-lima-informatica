95d6e1f6cddf1a8b80e9d7ea2511da4c0b8d7ead
Uítalo <uitalo.urss@gmail.com>
2022-02-07 01:07:48 -0300

    ajustando o header para todos os tipos de tela

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 import { initScrollReveal } from "./scrollReveal.js";
-
 initScrollReveal();
+
+const btnMobile = document.querySelector('#btn-mobile');
+btnMobile.addEventListener('click',() =>{
+  const nav = document.querySelector('.nav');
+  nav.classList.toggle("active");
+  changeButton(btnMobile);
+})
+
+const changeButton = (button) =>{
+  if(button.classList.contains("fa-bars")){
+    button.classList.remove("fa-bars");
+    button.classList.add("fa-times");
+  }else{
+    button.classList.remove("fa-times");
+    button.classList.add("fa-bars");
+  }
+}
+
